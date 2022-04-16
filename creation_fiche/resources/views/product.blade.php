39085da88057a72ba8b138e8c6251864d360c9af
Majonnie <bonhommejulie2003@gmail.com>
2022-04-17 01:17:45 +0200

    css fiches produits fini (revoir éventuelles erreurs)

--- creation_fiche/resources/views/product.blade.php
+++ creation_fiche/resources/views/product.blade.php
@@ -10,30 +10,30 @@
 
     <body>
         <div class="main_wrapper">
-            <h2>[FICHE PRODUIT]</h2>
+            {{-- <h2>[FICHE PRODUIT]</h2> --}}
             <div class="product_div">
                 {{-- Si le produit est soldé, un bandeau est ajouté pour l'indiquer à l'utilisateur --}}
                 @if($product->is_on_sale == 1)
                     <h2 class="on_sale">EN SOLDE !</h2>
                 @endif
 
-                <h2 class="product_name"><b>{{$product->name}}</b></h2>
+                <div class="base">
+                    <h2 class="product_name"><b>{{$product->name}}</b></h2>
 
-                <div class="product_image">
-                    <img src="{{asset('images/products/'.$product->image.'.png')}}" alt="{{$product->name}}">
-                </div>
-                <div class="product_info">
-                    <div>
-                        <p>{{$product->price}} €</p>
-                        <p>{{$product->description}}</p>
-                        <p>Quantité : {{$product->quantity}}</p>
-                        <p>Catégorie : {{$product->category}}</p>
-                        <p>Référence : {{$product->reference}}</p>
+                    <div class="product_image">
+                        <img src="{{asset('images/products/'.$product->image.'.png')}}" alt="{{$product->name}}">
                     </div>
-
-                    <a href="{{url('catalogue')}}">Retour au catalogue</a>
-
                 </div>
+
+                <div class="product_info">
+                    <p>{{$product->price}} €</p>
+                    <p>{{$product->description}}</p>
+                    <p>Quantité : {{$product->quantity}}</p>
+                    <p>Catégorie : {{$product->category}}</p>
+                    <p>Référence : {{$product->reference}}</p>
+                </div>
+
+                <a class="link" href="{{url('catalogue')}}">Retour au catalogue</a>
             </div>
         </div>
 
